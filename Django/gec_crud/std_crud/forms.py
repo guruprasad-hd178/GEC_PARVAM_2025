c2630e8451e0c08b6be4485babcb9b20d7264d4c
guruprasad-hd178 <guruprasad.parvam@gmail.com>
2025-03-12 20:06:23 +0530

    Django project

---
+++ Django/gec_crud/std_crud/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from . import models
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = models.Student
+        fields = ["name","email","password"]
+        widgets = {
+            "name" : forms.TextInput(attrs = {"class" : "form-control", "placeholder": "Enter your name"}),
+            'email' : forms.EmailInput(attrs = {"class" : "form-control", "placeholder": "Enter your email"}),
+            'password' : forms.PasswordInput(attrs = {"class" : "form-control", "placeholder": "Enter your password"}),
+        }
