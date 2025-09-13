986abcd3ad1527ed4ca1d88816252efe10442715
GURUPRASAD H D <gurudilp456@gmail.com>
2025-09-13 22:41:03 +0530

    template inheritance

---
+++ Core Java/SpringBootTemplateInheritence/src/main/java/com/springboottemplateinheritence/SpringBootTemplateInheritenceApplication.java
@@ -0,0 +1,13 @@
+package com.springboottemplateinheritence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTemplateInheritenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTemplateInheritenceApplication.class, args);
+	}
+
+}
---
+++ Core Java/SpringBootTemplateInheritence/src/test/java/com/springboottemplateinheritence/SpringBootTemplateInheritenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboottemplateinheritence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTemplateInheritenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
